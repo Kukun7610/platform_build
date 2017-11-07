ef2dc230673ec38eb913b9247f9d957eed9a659a
Kukun Mishra <kukun7610@gmail.com>
2017-11-07 10:35:02 +0530

    make soong suitable for DOSP

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -16,8 +16,8 @@ endif
 # Create soong.variables with copies of makefile settings.  Runs every build,
 # but only updates soong.variables if it changes
 SOONG_VARIABLES_TMP := $(shell mktemp -u)
-include vendor/aosp/build/soong/soong_config.mk
-$(SOONG_VARIABLES): FORCE aosp_soong
+include vendor/dosp/build/soong/soong_config.mk
+$(SOONG_VARIABLES): FORCE dosp_soong
 	$(hide) (\
 	echo '    "Make_suffix": "-$(TARGET_PRODUCT)",'; \
 	echo ''; \
